f82f78bbd41362e1a3addaedb81cbf99b116c1f0
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2024-01-05 23:25:44 +0300

    First commit - includes set up and first 30 min

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header"
+
+const Layout = () => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+        </div>
+    )
+}
