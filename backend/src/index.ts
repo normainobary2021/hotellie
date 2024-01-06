1e1ec391b2c819054bc790b5634770a1d8fcbddc
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2024-01-06 23:12:36 +0300

    update users routes and user model file, create first user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,9 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/test', async (req: Request, res: Response) => {
-    res.json({ message: 'Hello from express endpoint!' });
-});
+// no longer needed
+// app.get('/api/test', async (req: Request, res: Response) => {
+//     res.json({ message: 'Hello from express endpoint!' });
+// });
+
+app.use("/api/users", userRoutes);
+
 
 app.listen(7000, ()=> {
   console.log('Server is running on http://localhost:7000');
