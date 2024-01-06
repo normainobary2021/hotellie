8d96839e07c8d23d79c19fe9dc75871be9a05656
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2024-01-07 00:09:03 +0300

    Complete login endpoint functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
-
+import authRoutes from './routes/auth';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -17,6 +17,7 @@ app.use(cors());
 //     res.json({ message: 'Hello from express endpoint!' });
 // });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 
