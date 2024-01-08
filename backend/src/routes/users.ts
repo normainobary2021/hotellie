5c5cc49ac5195661cca8e10a76ddb82ae5ac658e
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2024-01-08 16:34:01 +0300

    Finalize User Registration Form and Endpoint Connection

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -30,7 +30,7 @@ async (req: Request, res: Response) => {
         
         res.cookie("auth_token", token, {httpOnly: true, secure: process.env.NODE_ENV === "production", maxAge: 86400000,});
 
-        return res.sendStatus(200);
+        return res.status(200).send({ message: "User registered OK" });
 
     } catch (error) {
         console.error(error);
