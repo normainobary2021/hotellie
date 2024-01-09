5de392786d3ba7216f5c13d1eba8f7642cab19e0
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2024-01-09 13:17:29 +0300

    Add appcontext and toast notification functionality

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     firstName: string;
@@ -11,15 +13,22 @@ export type RegisterFormData = {
 }
 
 const Register = () => {
+    
+    const navigate = useNavigate();
 
+    const { showToast } = useAppContext();
+    
     const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
     const mutation = useMutation(apiClient.register, {
         onSuccess: () => {
-            console.log("User registered successfully");
+            // console.log("User registered successfully");
+            showToast({message: "User registered successfully", type: "SUCCESS"});
+            navigate("/");
         },
         onError: (error: Error) => {
-            console.log(error.message);
+            // console.log(error.message);
+            showToast({message: error.message, type: "ERROR"});
         }
     })
 
