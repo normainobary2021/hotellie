2f3c9fa4ac02e32e7731b400d761e4bf74fd0be4
Norman Ainobushoborozi <norman.ainobushoborozi@gmail.com>
2024-01-10 21:29:31 +0300

    Finish Sign In - Sign up

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,4 +1,5 @@
 import { RegisterFormData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -19,6 +20,25 @@ export const register = async (formData: RegisterFormData) => {
     }
 }
 
+export const signIn = async (formData: SignInFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(formData),
+    });
+
+    const responseBody = await response.json();
+
+    if(!response.ok) {
+        throw new Error(responseBody.message)
+    }
+
+    return responseBody;
+}
+
 export const validateToken =async () => {
     const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
         credentials: "include"
@@ -30,3 +50,14 @@ export const validateToken =async () => {
 
     return response.json();
 }
+
+export const signOut = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+        method: "POST",
+        credentials: "include"
+    });
+
+    if(!response.ok) {
+        throw new Error("Something went wrong")
+    }
+};
---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from '../api-client';
+import { useAppContext } from "../contexts/AppContext";
+
+const SignOutButton = () => {
+    const queryClient = useQueryClient();
+    const { showToast } = useAppContext();
+
+    const mutation = useMutation(apiClient.signOut, {
+        onSuccess: async () => {
+            await queryClient.invalidateQueries("validateToken");
+            // show toast
+            showToast({message: "Signed out!", type: "SUCCESS"});
+        },
+        onError: (error: Error) => {
+            // show toast
+            showToast({ message: error.message, type: "ERROR"});
+        },
+    });
+
+    const handleClick = () => {
+        mutation.mutate();
+    }
+    return (
+        <button onClick={handleClick} className="bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100">Sign Out</button>
+    )
+};
+
+export default SignOutButton;
